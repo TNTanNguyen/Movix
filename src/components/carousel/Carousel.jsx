301c63acf204c90b4afea0d05e55a85020dc8a72
Tan Nguyen <nobitannguyen2014@gmail.com>
2023-03-18 11:16:07 +0700

    fix carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,21 +1,19 @@
+import dayjs from "dayjs";
 import React, { useRef } from "react";
 import {
   BsFillArrowLeftCircleFill,
-  BsFillArrowRightCircleFill,
+  BsFillArrowRightCircleFill
 } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
 
+import PosterFallback from "../../assets/no-poster.png";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
-import PosterFallback from "../../assets/no-poster.png";
-// import CircleRating from "../circleRating/CircleRating";
-// import Genres from "../genres/Genres";
 
-import "./style.scss";
-import CircleRating from "../cricleRating/CricleRating";
+import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
+import "./style.scss";
 
 const Carousel = ({ data, loading,endpoint,title }) => {
   const carouselContainer = useRef(null);
